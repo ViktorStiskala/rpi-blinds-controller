556698e1863dcca5d28c143f45b990746f2f0bc4
Viktor Stískala <viktor@stiskala.cz>
2016-02-21 22:19:28 +0000

    First API prototype

--- app.py
+++ app.py
@@ -1,39 +1,77 @@
-import RPi.GPIO as GPIO
+from blinds import Blinds
 import time
+from flask import Flask, jsonify, make_response, request
+from flask.views import View
 
-BOARD_SLEEP_TIME = 0.06
+app = Flask(__name__)
 
-PIN_CHANNEL = 3
-PIN_DOWN = 5
-PIN_MY = 7
-PIN_UP = 8
-
-def sleep():
-    time.sleep(BOARD_SLEEP_TIME)
-
-def trigger(channel):
-    GPIO.output(channel, GPIO.LOW)
-    sleep()
-    GPIO.output(channel, GPIO.HIGH)
-    sleep()
-
-def set_channel(current, final):
-    if current == final:
-        return
-
-    trigger_count = ((final+5) - current) % 5
-    for i in range(trigger_count + 1):
-        trigger(PIN_CHANNEL)
-
-try:
-    GPIO.setmode(GPIO.BOARD)
-
-    GPIO.setup(PIN_CHANNEL, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.setup(PIN_DOWN, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.setup(PIN_MY, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.setup(PIN_UP, GPIO.OUT, initial=GPIO.HIGH)
+directions = {
+    'up': 'trigger_up',
+    'down': 'trigger_down',
+    'my': 'trigger_my'
+}
 
 
-    set_channel(4, 3)
-finally:
-    GPIO.cleanup()
+@app.route('/blinds/channel/<int:channel>/<direction>/', methods=['PUT'])
+def blinds_move(channel, direction):
+    blinds = app.config['blinds']
+
+    channel -= 1
+    if channel < 0 or channel > 4:
+        return make_response(jsonify({'status': 'error', 'reason': 'invalid channel'}), 400)
+
+    try:
+        trigger = getattr(blinds, directions[direction])
+    except KeyError:
+        return make_response(jsonify({'status': 'error', 'reason': 'invalid direction'}), 400)
+
+    blinds.set_channel(channel)
+    trigger()
+    return jsonify({'status': 'ok'})
+
+
+@app.route('/blinds/channel/<int:channel>/', methods=['POST'])
+def blinds_short_move(channel):
+    blinds = app.config['blinds']
+
+    channel -= 1
+    if channel < 0 or channel > 4:
+        return make_response(jsonify({'status': 'error', 'reason': 'invalid channel'}), 400)
+
+    data = request.get_json()
+    try:
+        direction = data['direction']
+        duration = int(data['duration'])
+    except (KeyError, ValueError):
+        return make_response(jsonify({
+            'status': 'error',
+            'reason': 'Invalid payload. Please check Content-Type: application/json and payload data'
+        }), 400)
+
+    if direction == 'up':
+        trigger = blinds.trigger_up
+    elif direction == 'down':
+        trigger = blinds.trigger_down
+    else:
+        return make_response(jsonify({'status': 'error', 'reason': 'invalid direction'}), 400)
+
+    if not 0 < duration < 2000:
+        return make_response(jsonify({'status': 'error', 'reason': 'duration have to be between 0 and 2000'}), 400)
+
+    trigger()
+    time.sleep(duration / 1000)
+    blinds.trigger_my()
+
+    return jsonify({'status': 'ok'})
+
+if __name__ == '__main__':
+    try:
+        blinds = Blinds('/var/run/blinds.channel')
+        app.config.update(
+            blinds=blinds
+        )
+
+        app.run(host='0.0.0.0')
+    finally:
+        Blinds.cleanup()
+
